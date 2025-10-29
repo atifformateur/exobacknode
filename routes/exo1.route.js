ac0f336d26d69a321bafd80dd400fa3c09c350ee
atifformateur <atif.zourgani@laplateforme.io>
2025-10-29 11:12:21 +0100

    first commit

---
+++ routes/exo1.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//route post sur exo1
+router.post('/exo1', (req, res)=> {
+    const data = req.body;
+    
+    res.status(200).json({
+        message: 'renvois des datas ok',
+        data: data
+    })
+})
+
+module.exports = router;
